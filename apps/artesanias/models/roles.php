2840e6f366e82d3435b6af78e9c6bb3d2d0a00d6
Bralio <l17920315@voaxaca.tecnm.mx>
2021-01-15 15:05:50 -0600

    version1

--- apps/artesanias/models/roles.php
+++ apps/artesanias/models/roles.php
@@ -1,11 +1,12 @@
 <?php
-class Clasificacion extends DAO {
+class Roles extends DAO {
 
     public function __construct() {
         $this->keyfield = "id";
         $this->id = 0;
         $this->descripcion = "";
-        $this->padre = 0;
+      
     } 
 }
+
 ?>
